5adac4c1e7f366e89fe231ad19fd5b8ded57a128
Rachel Skwerer <rskwerer@affirma.com>
2022-07-06 16:35:15 -0500

    Initial Commit

---
+++ Shared/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  Shared
+//
+//  Created by Affirma Interns on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
