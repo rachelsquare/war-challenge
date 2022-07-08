5fa750f8866879e55f17c05567bf6a934596603b
Rachel Skwerer <rskwerer@affirma.com>
2022-07-08 13:27:02 -0500

    added functionality to game

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
         ZStack {
             Image("background").ignoresSafeArea()
@@ -20,13 +26,37 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("dealbutton")
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    
+                    if playerRand > cpuRand {
+                        playerScore+=1
+                    }
+                    else if playerRand < cpuRand {
+                        cpuScore+=1
+                    }
+                    
+                }, label: {
+                    
+                    Image("dealbutton")
+                })
+             
                 Spacer()
                 HStack {
                     Spacer()
@@ -34,7 +64,7 @@ struct ContentView: View {
                         Text("Player")
                             .padding(.bottom, 10)
                             .font(.headline)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -42,7 +72,7 @@ struct ContentView: View {
                         Text("CPU")
                             .padding(.bottom, 10)
                             .font(.headline)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
